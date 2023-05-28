e726c496d5a2e0cb78a179ab3aadebaae8a97b8d
Osmar Ortiz <osmarortiz0004@gmail.com>
2023-05-28 16:55:55 -0600

    Se crea el formulario de eliminar y se agrega el metodo sumar_costo_al_presupuesto

--- proyectofinal2023/costos/signals.py
+++ proyectofinal2023/costos/signals.py
@@ -1,20 +1,11 @@
-# from django.db.models.signals import pre_save
-# from django.dispatch import receiver
-# from .models import Costo
-# from django.db import models
-# from django.contrib import messages
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from .models import Costo
 
-# def actualizar_presupuesto(sender, instance, **kwargs):
-#     proyecto = instance.proyecto
-#     costos_totales = Costo.objects.filter(proyecto=proyecto).aggregate(total=models.Sum('costo'))['total']
-#     print(str(proyecto.presupuesto), str(costos_totales))
-#     presupuesto_actual = float(proyecto.presupuesto) - float(costos_totales)
-#     proyecto.presupuesto =  presupuesto_actual
-#     if presupuesto_actual < 0 :
-#         messages.warning(instance.request, "¡El presupuesto ha sido superado!")
-#         pass
-        
-#     else:
-#         proyecto.save() 
-
-# pre_save.connect(actualizar_presupuesto, sender=Costo)
+# Función de señal que se ejecutará antes de eliminar un objeto Costo
+@receiver(pre_delete, sender=Costo)
+def sumar_costo_al_presupuesto(sender, instance, **kwargs):
+    proyecto = instance.proyecto
+    costo = instance.costo
+    proyecto.presupuesto = float(proyecto.presupuesto) + float(costo)
+    proyecto.save()
