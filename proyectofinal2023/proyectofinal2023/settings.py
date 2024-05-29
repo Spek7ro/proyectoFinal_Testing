9a5b2cc8c320b79e389e207ad60bea67d5163125
cristian <20201032@uaz.edu.mx>
2024-05-28 21:33:10 -0600

    Configuracion de la base de datos

--- proyectofinal2023/proyectofinal2023/settings.py
+++ proyectofinal2023/proyectofinal2023/settings.py
@@ -84,13 +84,23 @@ WSGI_APPLICATION = 'proyectofinal2023.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),        
+        'PORT': '3306',   
     }
-}
-
+}	
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
