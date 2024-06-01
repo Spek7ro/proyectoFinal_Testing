93b26af2b56a19ff1be30df3e31d3d50567c4b7b
osmar <osmarortiz0004@gmail.com>
2024-06-01 03:47:14 -0600

    Se agregan las carpetas de pruebas a cada app del proyecto eliminando los archivos test.py de cada proyecto

--- proyectofinal2023/proveedores/tests/test_models.py
+++ proyectofinal2023/proveedores/tests/test_models.py
@@ -2,18 +2,43 @@ from django.test import TestCase
 from proveedores.models import Proveedor, Estado, Municipio
 
 class TestProveedorModels(TestCase):
-    def test_crear_proveedor(self):
-        estado = Estado.objects.create(nombre='Estado de México')
-        municipio = Municipio.objects.create(nombre='Municipio de México', estado=estado)
-        proveedor = Proveedor.objects.create(
+    def setUp(self):
+        self.proveedor = Proveedor(
             rfc='123456789',
             razon_social='Proveedora',
             direccion='Dirección',
             telefono='1234567890',
             correo='correo@correo',
-            estado=estado,
-            municipio=municipio
+            estado=Estado.objects.create(nombre='Estado de México'),
+            municipio=Municipio.objects.create(nombre='Municipio de México', estado=Estado.objects.create(nombre='Estado de México'))
         )
-        self.assertEqual(proveedor.rfc, '123456789')
-        self.assertEqual(proveedor.razon_social, 'Proveedora')
-        self.assertEqual(proveedor.direccion, 'Dirección')
+        
+        self.estado = Estado.objects.create(nombre='Estado de México')
+        
+        self.municipio = Municipio.objects.create(nombre='Municipio de México', estado=self.estado)
+        
+    def test_crear_proveedor(self):
+        self.assertEqual(self.proveedor.rfc, '123456789')
+        self.assertEqual(self.proveedor.razon_social, 'Proveedora')
+        self.assertEqual(self.proveedor.direccion, 'Dirección')
+        
+    def test_crear_estado(self):
+        self.assertEqual(self.estado.nombre, 'Estado de México')
+        
+    def test_crear_municipio(self):
+        self.assertEqual(self.municipio.nombre, 'Municipio de México')
+        self.assertEqual(self.municipio.estado, 'Estado de México')
+        
+    def test_crear_proveedor_datos_invalido(self):
+        estado = Estado.objects.create(nombre='Estado de México')
+        municipio = Municipio.objects.create(nombre='Municipio de México', estado=estado)
+        with self.assertRaises(ValueError):
+            Proveedor.objects.create(
+                rfc='123456789',
+                razon_social='Proveedora',
+                direccion='Dirección',
+                telefono='1234567890',
+                correo='correo@correo',
+                estado=estado,
+                municipio=municipio
+            )
